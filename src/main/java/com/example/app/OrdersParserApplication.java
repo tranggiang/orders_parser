ea4572b6f96d6532d1174e66048cc8033825feb7
tranggiang <twowaydown@gmail.com>
2020-11-13 22:21:32 +0700

    commit first time

---
+++ src/main/java/com/example/app/OrdersParserApplication.java
@@ -0,0 +1,16 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersParserApplication.class, args);
+		
+		System.out.println("running application");
+		
+	}
+
+}
